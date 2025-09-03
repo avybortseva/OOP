641ea171a23c8aecb428fa29d3acd50d9ea2171a
avybortseva <avybortseva0709@gmail.com>
2025-09-03 15:34:54 +0700

    add sort and tests

--- Task_1_1_1/src/main/java/ru/nsu/g/a/vybortseva/sort/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/g/a/vybortseva/sort/Sort.java
@@ -4,7 +4,51 @@ package ru.nsu.g.a.vybortseva.sort;
  * Описание класса.
  */
 public class Sort {
-    public static int[] sort(int[] array) {
-        return new int[]{1, 2, 3};
+    /**
+     * Описание.
+     */
+    public static int[] heapsort(int[] array) {
+        int len = array.length;
+
+        if (len <= 1) {
+            return array;
+        }
+
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            heapify(array, i, len);
+        }
+
+        for (int i = len - 1; i > 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, 0, i);
+        }
+
+        return array;
+    }
+
+    /**
+     * Описание.
+     */
+    private static void heapify(int[] array, int root, int len) {
+        int maxim = root;
+        int left = root * 2 + 1;
+        int right = root * 2 + 2;
+
+        if (left < len && array[left] > array[maxim]) {
+            maxim = left;
+        }
+        if (right < len && array[right] > array[maxim]) {
+            maxim = right;
+        }
+        if (maxim != root) {
+            int temp = array[root];
+            array[root] = array[maxim];
+            array[maxim] = temp;
+
+            heapify(array, maxim, len);
+        }
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/g/a/vybortseva/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/g/a/vybortseva/sort/SortTest.java
@@ -7,9 +7,85 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortTest {
 
     @Test
-    void sort() {
-        int[] array = new int[]{1, 3, 2};
-        var result = Sort.sort(array);
-        assertArrayEquals(new int[]{1, 2, 3}, result);
+    void test1() {
+        int[] array = new int[]{};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{}, result);
     }
+
+    @Test
+    void test2() {
+        int[] array = new int[]{1};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1}, result);
+    }
+
+    @Test
+    void test3() {
+        int[] array = new int[]{1, 2};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2}, result);
+    }
+
+    @Test
+    void test4() {
+        int[] array = new int[]{2, 1};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2}, result);
+    }
+
+    @Test
+    void test5() {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, result);
+    }
+
+    @Test
+    void test6() {
+        int[] array = new int[]{7, 6, 5, 4, 3, 2, 1};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, result);
+    }
+
+    @Test
+    void test7() {
+        int[] array = new int[]{3, 1, 4, 1, 5, 9, 2, 6, 6, 28, 39, 10};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 1, 2, 3, 4, 5, 6, 6, 9, 10, 28, 39}, result);
+    }
+
+    @Test
+    void test8() {
+        int[] array = new int[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7}, result);
+    }
+
+    @Test
+    void test9() {
+        int[] array = new int[]{-4, -3, -7, -1, -20};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{-20, -7, -4, -3, -1}, result);
+    }
+
+    @Test
+    void test10() {
+        int[] array = new int[]{-1, 6, -3, -9, 3, -4, 3, 4, 5, -3};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{-9, -4, -3, -3, -1, 3, 3, 4, 5, 6}, result);
+    }
+
+    @Test
+    void test11() {
+        int[] array = new int[1000];
+        int[] expected = new int[1000];
+        for (int i = 0; i < 1000; i++) {
+            array[i] = 1000 - i;
+            expected[i] = i + 1;
+        }
+        var result = Sort.heapsort(array);
+        assertArrayEquals(expected, result);
+    }
+
 }
