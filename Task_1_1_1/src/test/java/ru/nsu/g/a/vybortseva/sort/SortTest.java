a2e013365e7d3d315abec44dc9064c6e7ab78345
avybortseva <avybortseva0709@gmail.com>
2025-09-04 22:48:48 +0700

    fix

--- Task_1_1_1/src/test/java/ru/nsu/g/a/vybortseva/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/g/a/vybortseva/sort/SortTest.java
@@ -9,71 +9,71 @@ class SortTest {
     @Test
     void emptyArray() {
         int[] array = new int[]{};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{}, array);
     }
 
     @Test
     void singleElement() {
         int[] array = new int[]{1};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1}, array);
     }
 
     @Test
     void twoElementsSorted() {
         int[] array = new int[]{1, 2};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2}, array);
     }
 
     @Test
     void twoElementsReversed() {
         int[] array = new int[]{2, 1};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2}, array);
     }
 
     @Test
     void sortedArray() {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, array);
     }
 
     @Test
     void reversedArray() {
         int[] array = new int[]{7, 6, 5, 4, 3, 2, 1};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, array);
     }
 
     @Test
     void arrayWithDuplicates() {
         int[] array = new int[]{3, 1, 4, 1, 5, 9, 2, 6, 6, 28, 39, 10};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 1, 2, 3, 4, 5, 6, 6, 9, 10, 28, 39}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 1, 2, 3, 4, 5, 6, 6, 9, 10, 28, 39}, array);
     }
 
     @Test
     void uniformArray() {
         int[] array = new int[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7}, array);
     }
 
     @Test
     void negativeNumbers() {
         int[] array = new int[]{-4, -3, -7, -1, -20};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{-20, -7, -4, -3, -1}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{-20, -7, -4, -3, -1}, array);
     }
 
     @Test
     void mixedSignNumbers() {
         int[] array = new int[]{-1, 6, -3, -9, 3, -4, 3, 4, 5, -3};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{-9, -4, -3, -3, -1, 3, 3, 4, 5, 6}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{-9, -4, -3, -3, -1, 3, 3, 4, 5, 6}, array);
     }
 
     @Test
@@ -84,8 +84,8 @@ class SortTest {
             array[i] = 1000 - i;
             expected[i] = i + 1;
         }
-        var result = Sort.heapsort(array);
-        assertArrayEquals(expected, result);
+        Sort.heapsort(array);
+        assertArrayEquals(expected, array);
     }
 
 }
