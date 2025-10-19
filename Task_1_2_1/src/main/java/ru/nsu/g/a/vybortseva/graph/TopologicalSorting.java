747c26de13fde6434c6571992c22762a7af09c0f
avybortseva <avybortseva0709@gmail.com>
2025-10-19 16:48:08 +0700

    fix

--- Task_1_2_1/src/main/java/ru/nsu/g/a/vybortseva/graph/TopologicalSorting.java
+++ Task_1_2_1/src/main/java/ru/nsu/g/a/vybortseva/graph/TopologicalSorting.java
@@ -2,9 +2,9 @@ package ru.nsu.g.a.vybortseva.graph;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
 
 /**
  * Класс для выполнения топологической сортировки ориентированного графа.
