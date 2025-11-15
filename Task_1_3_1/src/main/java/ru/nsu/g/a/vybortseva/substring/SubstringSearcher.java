bc5acf7d7ff311ae9c284b4f65b6ac270f52a8c4
avybortseva <avybortseva0709@gmail.com>
2025-11-15 16:17:49 +0700

    fix

--- Task_1_3_1/src/main/java/ru/nsu/g/a/vybortseva/substring/SubstringSearcher.java
+++ Task_1_3_1/src/main/java/ru/nsu/g/a/vybortseva/substring/SubstringSearcher.java
@@ -1,8 +1,8 @@
 package ru.nsu.g.a.vybortseva.substring;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
