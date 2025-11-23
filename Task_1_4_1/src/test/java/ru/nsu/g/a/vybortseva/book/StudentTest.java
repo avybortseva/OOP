62c53a9bbe191d9151847fa274e4459df209322f
avybortseva <avybortseva0709@gmail.com>
2025-11-23 17:00:54 +0700

    fix

--- Task_1_4_1/src/test/java/ru/nsu/g/a/vybortseva/book/StudentTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/g/a/vybortseva/book/StudentTest.java
@@ -53,7 +53,8 @@ class StudentTest {
     void getAcademicHistory() {
         assertTrue(student.getAcademicHistory().isEmpty());
 
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
         assertEquals(1, student.getAcademicHistory().size());
     }
 
@@ -65,13 +66,14 @@ class StudentTest {
 
     @Test
     void addAcademicRecord() {
-        AcademicRecord record = new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT);
+        AcademicRecord record = new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT);
         student.addAcademicRecord(record);
 
         List<AcademicRecord> history = student.getAcademicHistory();
         assertEquals(1, history.size());
-        assertEquals("Математика", history.getFirst().getDisciplineName());
-        assertEquals(Grade.EXCELLENT, history.getFirst().getGrade());
+        assertEquals("Математика", history.get(0).getDisciplineName());
+        assertEquals(Grade.EXCELLENT, history.get(0).getGrade());
     }
 
     @Test
@@ -98,42 +100,54 @@ class StudentTest {
 
     @Test
     void testGetAverageScore() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.EXAM, Grade.GOOD));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "История", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.EXAM, Grade.GOOD));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "История", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
 
         assertEquals(4.0, student.getAverageScore(), 0.01);
     }
 
     @Test
     void increasedScholarshipPossibility_BudgetStudentAllExcellent() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
 
         assertTrue(student.increasedScholarshipPossibility());
     }
 
     @Test
     void increasedScholarshipPossibility_BudgetStudentWithGoodGrade() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.DIFF_CREDIT, Grade.GOOD));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.DIFF_CREDIT, Grade.GOOD));
 
         assertFalse(student.increasedScholarshipPossibility());
     }
 
     @Test
     void increasedScholarshipPossibility_PaidStudent() {
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
 
         assertFalse(paidStudent.increasedScholarshipPossibility());
     }
 
     @Test
     void redDiplomaPossibility_AllConditionsMet() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Информатика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Алгоритмы", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Информатика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Алгоритмы", RecordType.DIFF_CREDIT, Grade.EXCELLENT));
 
         student.setThesisGrade(Grade.EXCELLENT);
         assertTrue(student.redDiplomaPossibility());
@@ -141,8 +155,10 @@ class StudentTest {
 
     @Test
     void redDiplomaPossibility_WithSatisfactoryGrade() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
         student.setThesisGrade(Grade.EXCELLENT);
 
         assertFalse(student.redDiplomaPossibility());
@@ -150,7 +166,8 @@ class StudentTest {
 
     @Test
     void redDiplomaPossibility_ThesisNotExcellent() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
         student.setThesisGrade(Grade.GOOD);
 
         assertFalse(student.redDiplomaPossibility());
@@ -158,9 +175,12 @@ class StudentTest {
 
     @Test
     void redDiplomaPossibility_WithRetake() {
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.GOOD));
-        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.GOOD));
+        student.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        student.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.EXAM, Grade.EXCELLENT));
         student.setThesisGrade(Grade.EXCELLENT);
 
         assertTrue(student.redDiplomaPossibility());
@@ -168,19 +188,26 @@ class StudentTest {
 
     @Test
     void budgetPossibility_PaidStudentNoSatisfactory() {
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.EXAM, Grade.GOOD));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Информатика", RecordType.EXAM, Grade.EXCELLENT));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Алгоритмы", RecordType.EXAM, Grade.GOOD));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.EXAM, Grade.GOOD));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Информатика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Алгоритмы", RecordType.EXAM, Grade.GOOD));
 
         assertTrue(paidStudent.budgetPossibility());
     }
 
     @Test
     void budgetPossibility_PaidStudentWithSatisfactory() {
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физика", RecordType.EXAM, Grade.SATISFACTORY));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Информатика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физика", RecordType.EXAM, Grade.SATISFACTORY));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Информатика", RecordType.EXAM, Grade.EXCELLENT));
 
         assertFalse(paidStudent.budgetPossibility());
     }
@@ -192,16 +219,21 @@ class StudentTest {
 
     @Test
     void budgetPossibility_LessThanTwoSessions() {
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
         assertFalse(paidStudent.budgetPossibility());
     }
 
     @Test
     void budgetPossibility_SatisfactoryInDiffCreditAllowed() {
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Математика", RecordType.EXAM, Grade.EXCELLENT));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER, "Физкультура", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "Информатика", RecordType.EXAM, Grade.EXCELLENT));
-        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER, "ОБЖ", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Математика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.FIRST_SEMESTER,
+                "Физкультура", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "Информатика", RecordType.EXAM, Grade.EXCELLENT));
+        paidStudent.addAcademicRecord(new AcademicRecord(Semester.SECOND_SEMESTER,
+                "ОБЖ", RecordType.DIFF_CREDIT, Grade.SATISFACTORY));
 
         assertTrue(paidStudent.budgetPossibility());
     }
