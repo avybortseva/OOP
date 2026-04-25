70a3618c1455078409637faaaf9ab640402d3b20
avybortseva <avybortseva0709@gmail.com>
2026-04-26 00:03:18 +0700

    fix

---
+++ Task_2_4_1/src/test/java/ru/nsu/g/a/vybortseva/model/TaskTest.java
@@ -0,0 +1,28 @@
+package ru.nsu.g.a.vybortseva.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getMaxPoints() {
+    }
+
+    @Test
+    void getSoftDeadline() {
+    }
+
+    @Test
+    void getHardDeadline() {
+    }
+}
